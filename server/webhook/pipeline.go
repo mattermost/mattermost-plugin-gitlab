625d3f5292191bab976d15b5fb3cb9bc7d5f20e5
Romain Maneschi <rmaneschi@gmail.com>
2019-05-02 15:45:45 +0200

    :ok_hand: from ifelse to switch: thanks @hanzei for review

--- server/webhook/pipeline.go
+++ server/webhook/pipeline.go
@@ -53,33 +53,34 @@ func (w *webhook) handleChannelPipeline(event *gitlab.PipelineEvent) ([]*HandleW
 	res := []*HandleWebhook{}
 	message := ""
 
-	if event.ObjectAttributes.Status == "running" {
+	switch event.ObjectAttributes.Status {
+	case "running":
 		message = fmt.Sprintf("[%s](%s) New pipeline by [%s](%s) for [%s](%s)", repo.PathWithNamespace, repo.WebURL, senderGitlabUsername, w.gitlabRetreiver.GetUserURL(senderGitlabUsername), event.Commit.Message, event.Commit.URL)
-	} else if event.ObjectAttributes.Status == "success" {
+	case "success":
 		message = fmt.Sprintf("[%s](%s) Pipeline by [%s](%s) success for [%s](%s)", repo.PathWithNamespace, repo.WebURL, senderGitlabUsername, w.gitlabRetreiver.GetUserURL(senderGitlabUsername), event.Commit.Message, event.Commit.URL)
-	} else if event.ObjectAttributes.Status == "failed" {
+	case "failed":
 		message = fmt.Sprintf("[%s](%s) Pipeline by [%s](%s) fail for [%s](%s)", repo.PathWithNamespace, repo.WebURL, senderGitlabUsername, w.gitlabRetreiver.GetUserURL(senderGitlabUsername), event.Commit.Message, event.Commit.URL)
+	default:
+		return res, nil
 	}
 
-	if len(message) > 0 {
-		toChannels := make([]string, 0)
-		subs := w.gitlabRetreiver.GetSubscribedChannelsForRepository(repo.PathWithNamespace, repo.Visibility == gitlab.PublicVisibility)
-		for _, sub := range subs {
-			if !sub.Pipeline() {
-				continue
-			}
-
-			toChannels = append(toChannels, sub.ChannelID)
+	toChannels := make([]string, 0)
+	subs := w.gitlabRetreiver.GetSubscribedChannelsForRepository(repo.PathWithNamespace, repo.Visibility == gitlab.PublicVisibility)
+	for _, sub := range subs {
+		if !sub.Pipeline() {
+			continue
 		}
 
-		if len(toChannels) > 0 {
-			res = append(res, &HandleWebhook{
-				From:       senderGitlabUsername,
-				Message:    message,
-				ToUsers:    []string{},
-				ToChannels: toChannels,
-			})
-		}
+		toChannels = append(toChannels, sub.ChannelID)
+	}
+
+	if len(toChannels) > 0 {
+		res = append(res, &HandleWebhook{
+			From:       senderGitlabUsername,
+			Message:    message,
+			ToUsers:    []string{},
+			ToChannels: toChannels,
+		})
 	}
 
 	return res, nil
