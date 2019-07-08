a27bfb6d74537a81fdcdc408fc97705574eac19c
Christopher Speller <crspeller@gmail.com>
2019-07-08 11:54:56 -0700

    Updating dependencies.

--- webapp/src/client/client.js
+++ webapp/src/client/client.js
@@ -7,101 +7,85 @@ export default class Client {
         this.url = `/plugins/${id}/api/v1`;
     }
 
-  getConnected = async (reminder = false) => {
-      return this.doGet(`${this.url}/connected?reminder=` + reminder);
-  };
+    getConnected = async (reminder = false) => {
+        return this.doGet(`${this.url}/connected?reminder=` + reminder);
+    };
 
-  getReviews = async () => {
-      return this.doGet(`${this.url}/reviews`);
-  };
+    getReviews = async () => {
+        return this.doGet(`${this.url}/reviews`);
+    };
 
-  getYourPrs = async () => {
-      return this.doGet(`${this.url}/yourprs`);
-  };
+    getYourPrs = async () => {
+        return this.doGet(`${this.url}/yourprs`);
+    };
 
-  getYourAssignments = async () => {
-      return this.doGet(`${this.url}/yourassignments`);
-  };
+    getYourAssignments = async () => {
+        return this.doGet(`${this.url}/yourassignments`);
+    };
 
-  getMentions = async () => {
-      return this.doGet(`${this.url}/mentions`);
-  };
+    getMentions = async () => {
+        return this.doGet(`${this.url}/mentions`);
+    };
 
-  getUnreads = async () => {
-      return this.doGet(`${this.url}/unreads`);
-  };
+    getUnreads = async () => {
+        return this.doGet(`${this.url}/unreads`);
+    };
 
-  getGitlabUser = async (userID) => {
-      return this.doPost(`${this.url}/user`, {user_id: userID});
-  };
+    getGitlabUser = async (userID) => {
+        return this.doPost(`${this.url}/user`, {user_id: userID});
+    };
 
-  doGet = async (url, body, headers = {}) => {
-      headers['X-Requested-With'] = 'XMLHttpRequest';
-      headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
+    doGet = async (url, body, headers = {}) => {
+        headers['X-Requested-With'] = 'XMLHttpRequest';
+        headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
 
-      try {
-          const response = await request.
-              get(url).
-              set(headers).
-              accept('application/json');
+        const response = await request.
+            get(url).
+            set(headers).
+            accept('application/json');
 
-          return response.body;
-      } catch (err) {
-          throw err;
-      }
-  };
+        return response.body;
+    };
 
-  doPost = async (url, body, headers = {}) => {
-      headers['X-Requested-With'] = 'XMLHttpRequest';
-      headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
+    doPost = async (url, body, headers = {}) => {
+        headers['X-Requested-With'] = 'XMLHttpRequest';
+        headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
 
-      try {
-          const response = await request.
-              post(url).
-              send(body).
-              set(headers).
-              type('application/json').
-              accept('application/json');
+        const response = await request.
+            post(url).
+            send(body).
+            set(headers).
+            type('application/json').
+            accept('application/json');
 
-          return response.body;
-      } catch (err) {
-          throw err;
-      }
-  };
+        return response.body;
+    };
 
-  doDelete = async (url, body, headers = {}) => {
-      headers['X-Requested-With'] = 'XMLHttpRequest';
-      headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
+    doDelete = async (url, body, headers = {}) => {
+        headers['X-Requested-With'] = 'XMLHttpRequest';
+        headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
 
-      try {
-          const response = await request.
-              delete(url).
-              send(body).
-              set(headers).
-              type('application/json').
-              accept('application/json');
+        const response = await request.
+            delete(url).
+            send(body).
+            set(headers).
+            type('application/json').
+            accept('application/json');
 
-          return response.body;
-      } catch (err) {
-          throw err;
-      }
-  };
+        return response.body;
+    };
 
-  doPut = async (url, body, headers = {}) => {
-      headers['X-Requested-With'] = 'XMLHttpRequest';
-      headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
+    doPut = async (url, body, headers = {}) => {
+        headers['X-Requested-With'] = 'XMLHttpRequest';
+        headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
 
-      try {
-          const response = await request.
-              put(url).
-              send(body).
-              set(headers).
-              type('application/json').
-              accept('application/json');
+        const response = await request.
+            put(url).
+            send(body).
+            set(headers).
+            type('application/json').
+            accept('application/json');
 
-          return response.body;
-      } catch (err) {
-          throw err;
-      }
-  };
+        return response.body;
+    };
 }
