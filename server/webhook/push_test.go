5f455369336c2f74ed397f88a199584bd0ded883
Hanzei <hanzei@mailbox.org>
2019-08-09 21:24:30 +0200

    Add test

--- server/webhook/push_test.go
+++ server/webhook/push_test.go
@@ -16,8 +16,7 @@ type testDataPushStr struct {
 	res             []*HandleWebhook
 }
 
-var testDataPush = []testDataPushStr{
-	{
+var testDataPush = []testDataPushStr{	{
 		testTitle: "manland push 1 commit",
 		fixture:   PushEvent,
 		gitlabRetreiver: newFakeWebhook([]*subscription.Subscription{
@@ -26,6 +25,20 @@ var testDataPush = []testDataPushStr{
 		res: []*HandleWebhook{{
 			Message: "[manland](http://my.gitlab.com/manland) has pushed 1 commit to [manland/webhook](http://localhost:3000/manland/webhook)\n" +
 				"- [really cool commit](http://localhost:3000/manland/webhook/commit/c30217b62542c586fdbadc7b5ee762bfdca10663)",
+			ToUsers:    []string{}, // No DM because user know he has push commits
+			ToChannels: []string{"channel1"},
+			From:       "manland",
+		}},
+	}, {
+		testTitle: "manland push 2 commits",
+		fixture:   pushEventWithTwoCommits,
+		gitlabRetreiver: newFakeWebhook([]*subscription.Subscription{
+			{ChannelID: "channel1", CreatorID: "1", Features: "pushes", Repository: "manland/webhook"},
+		}),
+		res: []*HandleWebhook{{
+			Message: "[manland](http://my.gitlab.com/manland) has pushed 2 commits to [manland/webhook](http://localhost:3000/manland/webhook)\n" +
+				"- [really cool commit](http://localhost:3000/manland/webhook/commit/c30217b62542c586fdbadc7b5ee762bfdca10663)\n"+
+				"- [another cool commit](http://localhost:3000/manland/webhook/commit/595f2a068cce60954565b224bc7c966c9e708cbf)",
 			ToUsers:    []string{}, // No DM because user know he has push commits
 			ToChannels: []string{"channel1"},
 			From:       "manland",
