97a08f6b2f704dbd562d7bd14d86c994726dbb90
Lev Brouk <lev@mattermost.com>
2019-10-29 15:14:00 -0700

    PR Feedback
    Also Go 1.13 compatible/tidy

--- Makefile
+++ Makefile
@@ -32,7 +32,7 @@ apply:
 
 ## Runs govet and gofmt against all packages.
 .PHONY: check-style
-check-style: webapp/.npminstall gofmt govet
+check-style: webapp/.npminstall gofmt govet errcheck
 	@echo Checking for style guide compliance
 
 ifneq ($(HAS_WEBAPP),)
--- build/custom.mk
+++ build/custom.mk
@@ -1,1 +1,9 @@
-# Include custom targets and environment variables here
+## Runs errcheck against all packages.
+.PHONY: errcheck
+errcheck:
+ifneq ($(HAS_SERVER),)
+	@echo Running errcheck
+	@# Workaroung because you can't install binaries without adding them to go.mod
+	env GO111MODULE=off $(GO) get github.com/kisielk/errcheck
+	errcheck ./server/...
+endif
