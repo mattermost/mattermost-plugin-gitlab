9f821f1e67985e1cc3ab68f9de9e6fc7e899b393
Eric Stevens <ericjaystevens@gmail.com>
2020-07-13 12:41:57 -0400

    Update server/command.go for err checking pattern
    Co-authored-by: Ben Schumacher <ben.schumacher@mattermost.com>

--- server/command.go
+++ server/command.go
@@ -400,10 +400,13 @@ func parseTriggers(triggersCsv string) *gitlab.AddWebhookOptions {
 
 func (p *Plugin) subscriptionDelete(info *gitlab.GitlabUserInfo, config *configuration, fullPath, channelID string) (string, error) {
 	normalizedPath := normalizePath(fullPath, config.GitlabURL)
-	if deleted, err := p.Unsubscribe(channelID, normalizedPath); err != nil {
+	deleted, err := p.Unsubscribe(channelID, normalizedPath);
+	if err != nil {
 		p.API.LogError("can't unsubscribe channel in command", "err", err.Error())
 		return "Encountered an error trying to unsubscribe. Please try again.", nil
-	} else if !deleted {
+	} 
+	
+	if !deleted {
 		return "Subscription not found, please check repository name.", nil
 	}
 
