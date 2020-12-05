de46ece13b8c9168c2beda3a8e9f0e6e7ed1e9f7
Nicolas Thumann <me@n-thumann.de>
2020-12-05 07:58:56 +0100

    [GH-184]: Use UserUsername in Link, Fixes GitLab id storage (#225)

--- server/command.go
+++ server/command.go
@@ -208,14 +208,16 @@ func (p *Plugin) ExecuteCommand(_ *plugin.Context, args *model.CommandArgs) (*mo
 		case SettingNotifications:
 			if value {
 				if err := p.storeGitlabToUserIDMapping(info.GitlabUsername, info.UserID); err != nil {
-					p.API.LogError("can't store GitLab user id mapping", "err", err.Error())
+					p.API.LogError("can't store GitLab to user id mapping", "err", err.Error())
 					return p.getCommandResponse(args, "Unknown error please retry or ask to an administrator to look at logs"), nil
 				}
-			} else {
-				if err := p.API.KVDelete(info.GitlabUsername + GitlabUsernameKey); err != nil {
-					p.API.LogError("can't delete GitLab username in kvstore", "err", err.Error())
+				if err := p.storeGitlabIDToUserIDMapping(info.GitlabUsername, info.GitlabUserID); err != nil {
+					p.API.LogError("can't store GitLab to GitLab id mapping", "err", err.Error())
 					return p.getCommandResponse(args, "Unknown error please retry or ask to an administrator to look at logs"), nil
 				}
+			} else if err := p.deleteGitlabToUserIDMapping(info.GitlabUsername); err != nil {
+				p.API.LogError("can't delete GitLab username in kvstore", "err", err.Error())
+				return p.getCommandResponse(args, "Unknown error please retry or ask to an administrator to look at logs"), nil
 			}
 			info.Settings.Notifications = value
 		case SettingReminders:
