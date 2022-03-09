a16bbcf246d551bcee6ffa5a7fef24807947d8c3
Lufty Abdillah <lufty.abdillah@gmail.com>
2022-03-09 08:24:16 +0700

    Removed user state and avatar url from response

--- server/gitlab/types.go
+++ server/gitlab/types.go
@@ -13,11 +13,9 @@ type TriggerPipelineResponse struct {
 	Tag         bool       `json:"tag"`
 	Username    string     `json:"username"`
 	Name        string     `json:"name"`
-	UserState   string     `json:"state"`
 	UpdatedAt   *time.Time `json:"updated_at"`
 	CommittedAt *time.Time `json:"committed_at"`
 	WebURL      string     `json:"web_url"`
-	AvatarURL   string     `json:"avatar_url"`
 }
 
 func newTriggerPipelineResponse(pipeline *internGitlab.Pipeline) *TriggerPipelineResponse {
@@ -28,11 +26,9 @@ func newTriggerPipelineResponse(pipeline *internGitlab.Pipeline) *TriggerPipelin
 		Tag:         pipeline.Tag,
 		Username:    pipeline.User.Username,
 		Name:        pipeline.User.Name,
-		UserState:   pipeline.User.State,
 		UpdatedAt:   pipeline.UpdatedAt,
 		CommittedAt: pipeline.CommittedAt,
 		WebURL:      pipeline.WebURL,
-		AvatarURL:   pipeline.User.AvatarURL,
 	}
 }
 
