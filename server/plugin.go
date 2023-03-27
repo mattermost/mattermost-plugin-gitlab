d11046844f80fb2bbb3a1faf5d2c8c2b1e856eef
Pantelis Vratsalis <pvratsalis@gmail.com>
2023-03-27 13:53:59 +0300

    [MM51547] Fix telemetry for gitlab and add account connected event

--- server/plugin.go
+++ server/plugin.go
@@ -99,11 +99,7 @@ func (p *Plugin) OnActivate() error {
 	}
 
 	p.initializeAPI()
-
-	p.telemetryClient, err = telemetry.NewRudderClient()
-	if err != nil {
-		p.API.LogWarn("Telemetry client not started", "error", err.Error())
-	}
+	p.initializeTelemetry()
 
 	p.oauthBroker = NewOAuthBroker(p.sendOAuthCompleteEvent)
 
@@ -128,6 +124,10 @@ func (p *Plugin) OnActivate() error {
 
 func (p *Plugin) OnDeactivate() error {
 	p.oauthBroker.Close()
+
+	if err := p.telemetryClient.Close(); err != nil {
+		p.client.Log.Warn("Telemetry client failed to close", "error", err.Error())
+	}
 
 	return nil
 }
