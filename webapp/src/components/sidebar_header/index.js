e6670646a2fd8ae24dbfdc561f80fe9fb8e14ba9
ayusht2810 <ayush.thakur@brightscout.com>
2024-03-27 11:34:30 +0530

    [MM-326] Fix issue of gitlab sidebar icons visible when user is not connected

--- webapp/src/components/sidebar_header/index.js
+++ webapp/src/components/sidebar_header/index.js
@@ -1,11 +1,13 @@
 import {connect} from 'react-redux';
 
 import SidebarHeader from './sidebar_header.jsx';
+import manifest from '../../manifest';
 
 function mapStateToProps(state) {
     const members = state.entities.teams.myMembers || {};
     return {
         show: Object.keys(members).length <= 1,
+        connected: state[`plugins-${manifest.id}`].connected,
     };
 }
 
