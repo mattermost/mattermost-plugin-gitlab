25b480277457eeeef730760ae4ee06e10add807e
Raghav Aggarwal <raghav.aggarwal@brightscout.com>
2024-11-29 12:55:21 +0530

    Convert files related to redux from js to ts (#462)
    * [MM-222] Convert reducer and action types file to typescript and removed some unused code
    
    * [MM-222] Converted select file to typescript and fixes some css warnings
    
    * [MM-222] Convert actions file to typescript
    
    * [MM-222] Updated reducer, used reducer type for store and added more types
    
    * [MM-222] Resolved conflicts from master
    
    * [MM-222] Migrate a component to TS and minor review fixes
    
    * [MM-222] Fix flaky test
    
    * [MM-520] Review fixes
    
    * [MM-520] Fix ts error in index file
    
    * [MM-542] Review fixes
    
    * MM-222: converted client file to TS
    
    * [MM-222]: Added type to the client promises
    
    * [MM-222]: Added more types to the client.ts file
    
    * [MM-222]: updated function return type and added few types to client.ts
    
    * [MM-222]: fixed linting issue
    
    * [MM-222]: resolved conflicts
    
    * [MM-222]: removed null type from client.ts API response
    
    * [MM-222]: Added some additional types
    
    * Added lint types
    
    * [MM-656] Add types for redux funcs related to create issue and attach commment modal
    
    * fixed lint of type any being used
    
    * [MM-222]: removed link tool tip data type
    
    * using single quote for strings
    
    ---------
    
    Co-authored-by: ayusht2810 <ayush.thakur@brightscout.com>
    Co-authored-by: kshitij katiyar <kshitij.katiyar@brightscout.com>

---
+++ webapp/src/components/user_attribute/index.ts
@@ -0,0 +1,41 @@
+import {connect} from 'react-redux';
+import {AnyAction, Dispatch, bindActionCreators} from 'redux';
+
+import {UserProfile} from 'mattermost-redux/types/users';
+
+import {getGitlabUser} from '../../actions';
+
+import {GlobalState, pluginStateKey} from 'src/types/store';
+
+import UserAttribute from './user_attribute';
+
+export type UserAttributeProps = UserAttributeStateProps & UserAttributeDispatchProps
+
+type UserAttributeDispatchProps = ReturnType<typeof mapDispatchToProps>;
+type UserAttributeStateProps = ReturnType<typeof mapStateToProps>;
+
+function mapStateToProps(state: GlobalState, ownProps: {user: UserProfile}) {
+    const idUser = ownProps.user ? ownProps.user.id : '';
+    const user = state[pluginStateKey].gitlabUsers[idUser] || {};
+    return {
+        id: idUser,
+        username: user.username,
+        gitlabURL: state[pluginStateKey].gitlabURL,
+    };
+}
+
+function mapDispatchToProps(dispatch: Dispatch<AnyAction>) {
+    return {
+        actions: bindActionCreators(
+            {
+                getGitlabUser,
+            },
+            dispatch,
+        ),
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(UserAttribute);
