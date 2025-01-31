95f6058205fcb865057a7d98badbaa8447d23c45
kshitij katiyar <kshitij.katiyar@brightscout.com>
2025-01-31 20:18:18 +0530

    fixed missing dependency

--- webapp/src/components/team_sidebar/team_sidebar.test.tsx
+++ webapp/src/components/team_sidebar/team_sidebar.test.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
 import React from 'react';
 
 import {describe, expect, test} from '@jest/globals';
