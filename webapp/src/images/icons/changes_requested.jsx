fa5857b7c5b7c0a4bcfddef7ed2f70a31c35bb18
Chris Gibson <c.a.gibson1990@gmail.com>
2025-02-05 18:21:51 +0000

    [GH-437] Update files in icon directory to typescript (#549)
    * Delete icon files in favor of importing icon directly
    
    * Fix code style

--- webapp/src/images/icons/changes_requested.jsx
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-import React from 'react';
-import {FileDiffIcon} from '@primer/octicons-react';
-
-export default () => (
-    <FileDiffIcon size='small'/>
-);
