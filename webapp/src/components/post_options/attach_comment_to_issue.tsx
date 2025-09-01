aa3e3aea954b5d997e59d22c6ab3a31e1bf4a576
Abbas Mehdi Naqvi <abbas.naqvi@brightscout.com>
2025-09-01 13:17:31 +0530

    Remove depricated registerPostDropdownMenuComponent (#599)
    * Remove depricated registerPostDropdownMenuComponent
    
    * Updated the component
    
    * update filter function
    
    * removed unused comments
    
    * use store to dispatch action
    
    * removed unused dependency

--- webapp/src/components/post_options/attach_comment_to_issue.tsx
+++ webapp/src/components/post_options/attach_comment_to_issue.tsx
@@ -1,60 +1,16 @@
 // Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {MouseEvent} from 'react';
-import PropTypes from 'prop-types';
-import {useDispatch, useSelector} from 'react-redux';
-import {getPost} from 'mattermost-redux/selectors/entities/posts';
-import {isSystemMessage} from 'mattermost-redux/utils/post_utils';
+import React from 'react';
 
 import GitLabIcon from 'src/images/icons/gitlab';
-import {openAttachCommentToIssueModal} from 'src/actions';
-import {getConnected} from 'src/selectors';
-import {GlobalState} from 'src/types/store';
 
-interface PropTypes {
-    postId: string;
-}
-
-const AttachCommentToIssuePostMenuAction = ({postId}: PropTypes) => {
-    const {show} = useSelector((state: GlobalState) => {
-        const post = getPost(state, postId);
-        const isPostSystemMessage = Boolean(!post || isSystemMessage(post));
-
-        return {
-            show: getConnected(state) && !isPostSystemMessage,
-        };
-    });
-
-    const dispatch = useDispatch();
-
-    const handleClick = (e: MouseEvent<HTMLButtonElement> | Event) => {
-        e.preventDefault();
-        dispatch(openAttachCommentToIssueModal(postId));
-    };
-
-    if (!show) {
-        return null;
-    }
-
-    const content = (
-        <button
-            className='style--none'
-            role='presentation'
-            onClick={handleClick}
-        >
+const AttachCommentToIssuePostMenuAction = () => {
+    return (
+        <>
             <GitLabIcon type='menu'/>
             {'Attach to GitLab Issue'}
-        </button>
-    );
-
-    return (
-        <li
-            className='MenuItem'
-            role='menuitem'
-        >
-            {content}
-        </li>
+        </>
     );
 };
 
